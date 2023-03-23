f6f2c5f57ca8ceb884189c3570a38702c95837d0
Frank Huang <karokwbh2000@gmail.com>
2023-03-23 13:26:30 +0800

    add navbar resume

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   get '/about', controller: 'pages', action: 'about'
   # 新式寫法
   get '/contact', to: 'pages#contact'
+
+  get '/resumes', to: 'resumes#index'
+  get '/resumes/new', to: 'resumes#new', as: 'new_resume'
+
+  # get '/', to: 'pages#home', as: 'root'
+  # 簡化 後期寫法
+  root 'pages#home'
 end
